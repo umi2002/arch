e925d5ce74bce7f13aacc1dd60aec8c6bd2bf9cd
umi2002 <jonathanhoang2002@gmail.com>
2023-05-16 22:47:23 -0400

    .

--- config/spicetify.sh
+++ config/spicetify.sh
@@ -3,7 +3,6 @@
 su umi << "EOF"
 export PATH=$PATH:/home/umi/.spicetify/
 cp -rf ./spotify/ ~/.config/
-cp -rf ./spicetify/ ~/.config/
 cd ~/srcpkgs/
 sudo chown /opt/spotify/
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
