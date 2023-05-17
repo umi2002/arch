68d7ad35f3f4a40dc76a15dadb680e8850607073
umi2002 <jonathanhoang2002@gmail.com>
2023-05-16 20:22:17 -0400

    .

--- installation/spicetify.sh
+++ installation/spicetify.sh
@@ -3,7 +3,6 @@
 su umi << "EOF"
 cd ~/srcpkgs/
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
-echo "export PATH=/home/umi/.spicetify/:$PATH" >> ~/.bashrc
 cd /arch
 cp -rf ../config/spicetify/ /home/umi/.config/
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
