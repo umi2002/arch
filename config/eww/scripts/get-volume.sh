d9bea0a74ff0e97d218eb79db9c57c41f04f4de9
umi2002 <jonathanhoang2002@gmail.com>
2023-06-29 13:38:01 -0400

    .

--- config/eww/scripts/get-volume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pactl subscribe | grep --line-buffered "sink #" | while read -r line; do pactl list sinks | grep '^[[:space:]]Volume:' | head -n 1 | sed 's|.*[[:space:]]\([[:digit:]]\+%\).*|\1|'; done
